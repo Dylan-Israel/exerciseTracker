c1377a374ba2a8058f16553db7802426e7018f49
Stephen Mayeux <stephenmayeux@gmail.com>
2017-03-31 23:27:04 -0500

    formatting

--- server.js
+++ server.js
@@ -3,39 +3,46 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require ('cors');
 const mongoose = require('mongoose');
+
 //Instantiate app
 const app = express();
-app.use(bodyParser.json());
-app.use(cors());
-//Require mongoDB models
-const user = require('./models/user');
-const exercise = require('./models/exercise');
+
 //Connect to the database
 mongoose.connect(process.env.MONGODB_URI  || 'mongodb://localhost/exercise');
+
+//Require mongoDB models
+const User = require('./models/user');
+const Exercise = require('./models/exercise');
+
+// include middleware
+app.use(bodyParser.json());
+app.use(cors());
+
 //Allow node to use public resources
 app.use(express.static(__dirname + '/public'));
+
 //Add a new user to the database
-app.post('/api/newUser/:userName', (req, res, next)=>{
-var {userName} = req.params;
-//Creates JSON based off 
-var data = new user({
-userName
-});
-//Saves to the user collection
-data.save(err=>{
-    if(err){
-        return res.send('Error saving to database');
-    }
-    return res.json(data);
-});
+app.post('/api/newUser/:userName', (req, res, next) => {
+  const {userName} = req.params;
+  //Creates JSON based off
+  const data = new user({
+    userName
+  });
+
+  //Saves to the user collection
+  data.save(err=>{
+    if (err) return res.status(500).send({ success: false, message: 'Error saving to database' });
+    return res.json({ success: true, data });
+  });
 
 });
 
-//Add and return exercise 
+//Add and return exercise
 app.get('/api/exercise/add/:id/:description/:duration/:date', (req, res, next)=>{
 
 });
 
-app.listen(process.env.PORT || 3000, () =>{
-    console.log('This shit works');
+const port = process.env.PORT || 3000;
+app.listen(port, () =>{
+  console.log('This shit works on port', port);
 });
